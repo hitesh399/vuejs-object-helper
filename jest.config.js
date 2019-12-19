fd59cfea062b3a8a66a994ce4a4e8c48f1fdabe3
Hitesh Kumar <hitesh.kumar399@gmail.com>
2019-12-20 00:16:47 +0530

    update

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,11 @@ module.exports = {
 
   "collectCoverage": true,
   "collectCoverageFrom": [
-    "**/*.{js,vue}",
+    "src/**/*.{js,vue}",
     "!**/node_modules/**"
   ],
   "coverageReporters": ["html", "text-summary"],
   "transform": {
     "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
-  }
-
+  },
 }
